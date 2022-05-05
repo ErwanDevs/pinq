a778b72b159b110f5782c69cff49e5b4ae9e913d
Erwan Cariou <erwan.creates@gmail.com>
2022-05-05 22:50:33 +0200

    Refactored to add base of project

---
+++ tests/test_queries/test_select.py
@@ -0,0 +1,42 @@
+import unittest
+from tests.utils import Utils
+from pinq.enumerable import Enumerable
+import pinq.query
+
+class TestSelect(unittest.TestCase):
+    def test_select_data_modification(self):
+        seed = 23
+        size = 100
+        refList = list(Utils.init_random_integer_list(seed, size))
+        modifiedList = Enumerable(refList).Select(lambda a : a+1)
+        for a, b in zip(refList, modifiedList):
+            self.assertEqual(a+1, b)
+
+    def test__select_successive_queries(self):
+        seed = 20
+        size = 130
+        refList = list(Utils.init_random_integer_list(seed, size))
+        modifiedList = Enumerable(refList).Select(lambda a : a+1).Select(lambda a : a -2)
+        for a, b in zip(refList, modifiedList):
+            self.assertEqual(a-1, b)
+
+    def test_select_long_query(self):
+        seed = 230
+        size = 1000
+        refList = list(Utils.init_random_integer_list(seed, size))
+        modifiedList = Enumerable(refList).Select(lambda a : a+1).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2).Select(lambda a : a -2).Select(lambda a : a +2)
+        for a, b in zip(refList, modifiedList):
+            self.assertEqual(a+1, b)
+
+    def test_select_with_index(self):
+        seed = 3487
+        size = 1000
+        refList = list(Utils.init_random_integer_list(seed, size))
+        modifiedList = Enumerable(refList).Select(lambda a, i : a+i)
+        i = 0
+        for a, b in zip(refList, modifiedList):
+            self.assertEqual(a+i, b)
+            i+=1
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_queries/test_where.py
@@ -0,0 +1,32 @@
+import unittest
+from pinq.enumerable import Enumerable
+from tests.utils import Utils
+from pinq.query import where
+
+class TestWhere(unittest.TestCase):
+    def test_where(self):
+        seed = 1
+        size = 1000
+        refList = list(Utils.init_random_integer_list(seed, size))
+        condition = lambda a : a < 50            
+        modifiedList = list(Enumerable(refList).Where(condition))
+        for a in refList:
+            if condition(a):
+                self.assertTrue(a in modifiedList)
+            else :
+                self.assertFalse(a in modifiedList)
+
+    def test_where_index(self):
+        seed = 50
+        size = 1000
+        refList = list(Utils.init_random_integer_list(seed, size))
+        condition = lambda number, index : number <= index * 10
+        modifiedList = list(Enumerable(refList).Where(condition))
+        for a, i in enumerate(refList):
+            if condition(a, i):
+                self.assertTrue(a in modifiedList)
+            else :
+                self.assertFalse(a in modifiedList)
+
+if __name__ == '__main__':
+    unittest.main()
